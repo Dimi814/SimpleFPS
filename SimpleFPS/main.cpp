f9608a72648b0197212ce50ca783305c3bfd1395
Dimitriy Dounaev <DimitriyDounaev@gmail.com>
2013-09-01 12:31:16 +0200

    Initial Commit

---
+++ SimpleFPS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SimpleFPS
+//
+//  Created by Dimitriy Dounaev on 1/09/13.
+//  Copyright (c) 2013 Dimitriy Dounaev. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
